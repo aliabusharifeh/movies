609866759f4718542dabe6677b0715dedf094467
aliabushareefah <>
2021-04-18 14:15:35 +0000

    newadd

---
+++ functiontest.js
@@ -0,0 +1,17 @@
+// // function expression getAge
+
+var name ;
+function getUserName() {
+   name = prompt('choose movie');
+ }
+
+
+var pntname = function () {
+console.log(name);
+ }
+getUserName();
+ //call the function
+ pntname();
+// // function declaration 
+
+
